7879c3aeb2aba6bb6697a203b5a719fb1634bf1a
kkh061101 <kkh061101@naver.com>
2023-01-18 20:43:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [list, setList] = useState([]);
+  const fetching = async () => {
+    try {
+      const date = new Date();
+      const today = (date.getFullYear().toString() + ('0' + (date.getMonth() + 1)).slice(-2).toString() + ('0' + date.getDate()).slice(-2).toString());
+      const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${today}`)).json();
+      setList(jsons.mealServiceDietInfo[1].row);
+      console.log(jsons.mealServiceDietInfo[1].row);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  useEffect(() => {
+    fetching();
+  }, []);
+  return <div>
+    {list.map((i, n) => <div key={n}>
+      {i.MMEAL_SC_NM}
+      {i.CAL_INFO}
+      <div>{i.NTR_INFO}</div>
+    </div>)}
+    asdf
+  </div>;
 }
 
 export default App;
