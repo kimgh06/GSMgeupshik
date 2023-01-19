b0c9165f453a15edbed5a5e332ca7ede5027f998
kkh061101 <kkh061101@naver.com>
2023-01-19 20:51:47 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ function App() {
   const [add, setAdd] = useState(0);
   const fetching = async (add) => {
     try {
-      setLoading(true);
+      setLoading(1);
       const date = new Date();
       const today = parseInt(date.getFullYear().toString() + ('0' + (date.getMonth() + 1)).slice(-2).toString() + ('0' + (parseInt(date.getDate()) + add).toString()).slice(-2).toString());
       const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${today}`)).json();
       setList(jsons.mealServiceDietInfo[1].row);
+      if (jsons.mealServiceDietInfo[1].row !== undefined) {
+        setLoading(0);
+      } else {
+        setLoading(-1);
+      }
       console.log(today, jsons);
-      setLoading(false);
     } catch (e) {
       console.log(e);
     }
@@ -23,7 +27,7 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div className="App">
-    {loading ? <span>loading</span> : <>
+    {loading !== 0 ? <span>loading</span> : <>
       <button onClick={() => {
         if (num < 1) {
           setAdd(c => c - 1);
