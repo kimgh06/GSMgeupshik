7bd64f474a19c777c9a9dcb9b09489293f034c30
kkh061101 <kkh061101@naver.com>
2023-01-19 20:32:01 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,37 +4,55 @@ function App() {
   const [list, setList] = useState([]);
   const [loading, setLoading] = useState(true);
   const [num, setNum] = useState(0);
-  const fetching = async () => {
+  const [add, setAdd] = useState(0);
+  const fetching = async (add) => {
     try {
       setLoading(true);
       const date = new Date();
-      const today = (date.getFullYear().toString() + ('0' + (date.getMonth() + 1)).slice(-2).toString() + ('0' + date.getDate()).slice(-2).toString());
+      const today = parseInt(date.getFullYear().toString() + ('0' + (date.getMonth() + 1)).slice(-2).toString() + ('0' + (parseInt(date.getDate()) + add).toString()).slice(-2).toString());
       const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${today}`)).json();
       setList(jsons.mealServiceDietInfo[1].row);
-      console.log(jsons.mealServiceDietInfo[1].row);
+      console.log(today, jsons);
       setLoading(false);
     } catch (e) {
       console.log(e);
     }
   }
   useEffect(() => {
-    fetching();
+    fetching(add);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div className="App">
     {loading ? <span>loading</span> : <>
-      <button onClick={() => setNum(e => e < 1 ? e : e - 1)}>◀</button>
+      <button onClick={() => {
+        if (num < 1) {
+          setAdd(c => c - 1);
+          fetching(add - 1);
+          setNum(2);
+        }
+        else {
+          setNum(c => c - 1);
+        }
+      }}>◀</button>
       <div className="Main">
         <p>{list[0].MLSV_YMD}</p>
-        {list.map((i, n) => <div key={n}>
-          <b>{i.MMEAL_SC_NM}</b>
-          {i.CAL_INFO}
-          {/* <div dangerouslySetInnerHTML={{ __html: i.NTR_INFO }} /> */}
-          <div dangerouslySetInnerHTML={{ __html: i.DDISH_NM }} />
-        </div>)}
+        <b>{list[num].MMEAL_SC_NM}&nbsp;</b>
+        <span>총{list[num].CAL_INFO}</span>
+        <div dangerouslySetInnerHTML={{ __html: list[num].DDISH_NM }} />
       </div>
-      <button onClick={() => setNum(e => (e + 1) % 3)}>▶</button>
-    </>}
-  </div>;
+      <button onClick={() => {
+        if (num > 1) {
+          setAdd(c => c + 1);
+          fetching(add + 1);
+          setNum(0);
+        }
+        else {
+          setNum(c => c + 1);
+        }
+      }}>▶</button>
+    </>
+    }
+  </div >;
 }
 
 export default App;
