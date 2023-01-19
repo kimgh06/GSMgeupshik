114a0f7fd8a6dc77b11d526fd4c517ddf25033c3
kkh061101 <kkh061101@naver.com>
2023-01-19 23:34:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [list, setList] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(1);
   const [num, setNum] = useState(0);
   const [add, setAdd] = useState(0);
   const fetching = async (add) => {
@@ -10,12 +10,14 @@ function App() {
       setLoading(1);
       const date = new Date();
       const today = parseInt(date.getFullYear().toString() + ('0' + (date.getMonth() + 1)).slice(-2).toString() + ('0' + (parseInt(date.getDate()) + add).toString()).slice(-2).toString());
-      const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${today}`)).json();
-      if (jsons.mealServiceDietInfo[1].row[0].MLSV_YMD !== undefined) {
-        setList(jsons.mealServiceDietInfo[1].row);
-        setLoading(0);
+      const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${20230123}`)).json();
+      if (JSON.stringify(jsons) !== JSON.stringify({ "RESULT": { "CODE": "INFO-200", "MESSAGE": "해당하는 데이터가 없습니다." } })) {
+        // setList(jsons.mealServiceDietInfo[1].row);
+        // setLoading(0);
+        console.log('asdf');
       } else {
-        setLoading(-1);
+        // setLoading(-1);
+        console.log('it has error');
       }
       console.log(today, jsons);
     } catch (e) {
@@ -27,7 +29,7 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div className="App">
-    {loading !== 0 ? <span>loading</span> : (loading === -1 ? <>i have a error</> : <>
+    {loading === 1 ? <span>loading</span> : (loading === -1 ? <>i have a error</> : <>
       <button onClick={() => {
         if (num < 1) {
           setAdd(c => c - 1);
