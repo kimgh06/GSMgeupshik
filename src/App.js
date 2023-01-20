7ed6270cd7b64bd97d096dee32c357e13d9da947
kkh061101 <kkh061101@naver.com>
2023-01-20 13:34:50 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
       setLoading(1);
       const date = new Date();
       const today = parseInt(date.getFullYear().toString() + ('0' + (date.getMonth() + 1)).slice(-2).toString() + ('0' + (parseInt(date.getDate()) + add).toString()).slice(-2).toString());
-      const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${20230123}`)).json();
+      const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${today}`)).json();
       if (JSON.stringify(jsons) !== JSON.stringify({ "RESULT": { "CODE": "INFO-200", "MESSAGE": "해당하는 데이터가 없습니다." } })) {
-        // setList(jsons.mealServiceDietInfo[1].row);
-        // setLoading(0);
+        setList(jsons.mealServiceDietInfo[1].row);
+        setLoading(0);
         console.log('asdf');
       } else {
-        // setLoading(-1);
+        setLoading(-1);
         console.log('it has error');
       }
       console.log(today, jsons);
