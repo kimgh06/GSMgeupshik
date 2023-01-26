ea62fd0a9115d0cecdd325d6a1dd41bac892c323
kkh061101 <kkh061101@naver.com>
2023-01-26 17:31:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [loading, setLoading] = useState(1);
   const [num, setNum] = useState(0);
   const [add, setAdd] = useState(0);
+  const [today, setToday] = useState();
   const fetching = async (add, num) => {
     try {
       setLoading(1);
@@ -13,7 +14,7 @@ function App() {
       let monAdd = 0;
       let month = ('0' + (date.getMonth() + 1)).slice(-2).toString();
       // dates < 1 ? :
-      const today = parseInt(date.getFullYear().toString() + month + dates);
+      setToday(parseInt(date.getFullYear().toString() + month + dates))
       const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${today}`)).json();
       if (JSON.stringify(jsons) !== JSON.stringify({ "RESULT": { "CODE": "INFO-200", "MESSAGE": "해당하는 데이터가 없습니다." } }) && num < jsons.mealServiceDietInfo[1].row.length) {
         setList(jsons.mealServiceDietInfo[1].row);
@@ -44,9 +45,9 @@ function App() {
         setNum(c => c - 1);
       }
     }}>◀</button>
-    {loading === 1 ? <span>loading</span> : (loading === -1 ? <>i have a error</> : <>
+    <p>{today}&nbsp;{num + 1}번째</p>
+    {loading === 1 ? <span>loading</span> : (loading === -1 ? <>급식이 없어요.</> : <>
       <div className="Main">
-        <p>{list[0].MLSV_YMD}</p>
         <b>{list[num].MMEAL_SC_NM}&nbsp;</b>
         <span>총{list[num].CAL_INFO}</span>
         <div dangerouslySetInnerHTML={{ __html: list[num].DDISH_NM }} />
