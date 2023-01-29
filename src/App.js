985be3856a0e49ec6338eece2bccc42f152d4106
kkh061101 <kkh061101@naver.com>
2023-01-29 23:49:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,34 +34,34 @@ function App() {
       let dates = ('0' + (parseInt(date.getDate()) + add).toString()).slice(-2).toString();
       let monAdd = 0;
       let month = ('0' + (date.getMonth() + 1)).slice(-2).toString();
-      if (parseInt(dates) < 1) { //감소할 경우
-        monAdd--;
-        month = parseInt(month);
-        if (whatmonths(month - 1) === 0) {
-          dates = (30 - (Math.abs(parseInt(dates)) % 30)).toString();
-        }
-        else if (whatmonths(month - 1) === 1) {
+      // if (parseInt(dates) < 1) { //감소할 경우
+      //   monAdd--;
+      //   month = parseInt(month);
+      //   if (whatmonths(month - 1) === 0) {
+      //     dates = (30 - (Math.abs(parseInt(dates)) % 30)).toString();
+      //   }
+      //   else if (whatmonths(month - 1) === 1) {
 
-        }
-        else if (whatmonths(month - 1) === 2) {
+      //   }
+      //   else if (whatmonths(month - 1) === 2) {
 
-        }
-        month += monAdd;
-      }
-      else if (date > 29) { //증가할 경우
-        month = parseInt(month);
-        if (whatmonths(month) === 0 && date > 30) {
-          monAdd++;
-          dates = (Math.floor(parseInt(dates) / 30)).toString();
-        }
-        else if (whatmonths(month) === 1) {
+      //   }
+      //   month = (month+ monAdd);
+      // }
+      // else if (parseInt(dates) > 29) { //증가할 경우
+      //   month = parseInt(month);
+      //   if (whatmonths(month) === 0 && date > 30) {
+      //     monAdd++;
+      //     dates = (Math.floor(parseInt(dates) / 30)).toString();
+      //   }
+      //   else if (whatmonths(month) === 1) {
 
-        }
-        else if (whatmonths(month) === 2) {
+      //   }
+      //   else if (whatmonths(month) === 2) {
 
-        }
-        month = ('0' + month).toString();
-      }
+      //   }
+      //   month = ('0' + month).toString();
+      // }
       setToday(parseInt(date.getFullYear().toString() + month + dates));
       const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${date.getFullYear().toString() + month + dates}`)).json();
       if (JSON.stringify(jsons) !== JSON.stringify({ "RESULT": { "CODE": "INFO-200", "MESSAGE": "해당하는 데이터가 없습니다." } }) && num < jsons.mealServiceDietInfo[1].row.length) {
@@ -72,7 +72,7 @@ function App() {
         setLoading(-1);
         console.log('it has error');
       }
-      console.log(today, jsons);
+      console.log(date.getFullYear().toString() + month + dates, jsons);
     } catch (e) {
       console.log(e);
     }
