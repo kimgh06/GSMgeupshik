f41a632c6ed277016117a799858ae7f5c1f38c98
kkh061101 <kkh061101@naver.com>
2023-01-30 15:18:01 +0900

    반환이 되지 않는 현상 수정

--- src/App.js
+++ src/App.js
@@ -8,22 +8,31 @@ function App() {
   const [add, setAdd] = useState(0);
   const [today, setToday] = useState();
   const whatmonths = (theMonth) => {
-    switch (parseInt(theMonth)) {
+    switch (parseInt(theMonth) % 12) {
+      case 1:
+        return 1; //31일
       case 2:
         return 2; //29일
-      case 1:
       case 3:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
-      case 12:
         return 1; //31일
       case 4:
+        return 0; //30일
+      case 5:
+        return 1; //31일
       case 6:
+        return 0; //30일
+      case 7:
+        return 1; //31일
+      case 8:
+        return 1; //31일
       case 9:
+        return 0; //30일
+      case 10:
+        return 1; //31일
       case 11:
         return 0; //30일
+      case 0:
+        return 1; //31일
       default:
         return -1; //머꼬?
     }
@@ -34,35 +43,37 @@ function App() {
       const date = new Date();
       let dates = ('0' + (parseInt(date.getDate()) + add).toString()).slice(-2).toString();
       let monAdd = 0;
-      let month = ('0' + (date.getMonth() + 1)).slice(-2).toString();
-      if (parseInt(dates) < 1) { //감소할 경우
-        monAdd--;
-        month = parseInt(month);
-        if (whatmonths(month - 1) === 0) {
-          dates = (30 - (Math.abs(parseInt(dates)) % 30)).toString();
-        }
-        else if (whatmonths(month - 1) === 1) {
+      let month = ('0' + (date.getMonth() + 1 + monAdd)).slice(-2).toString();
+      // if (parseInt(dates) < 1) { //감소할 경우
+      //   monAdd--;
+      //   month = parseInt(month);
+      //   if (whatmonths(month - 1) === 0) {
+      //     dates = (30 - (Math.abs(parseInt(dates)) % 30)).toString();
+      //   }
+      //   else if (whatmonths(month - 1) === 1) {
 
-        }
-        else if (whatmonths(month - 1) === 2) {
+      //   }
+      //   else if (whatmonths(month - 1) === 2) {
 
-        }
-        month = (month + monAdd);
-      }
-      else if (parseInt(dates) > 29) { //증가할 경우
-        month = parseInt(month);
-        if (whatmonths(month) === 0 && date > 30) {
-          monAdd++;
-          dates = (Math.floor(parseInt(dates) / 30)).toString();
-        }
-        else if (whatmonths(month) === 1) {
+      //   }
+      //   month = (month + monAdd);
+      // }
+      // else if (parseInt(dates) > 29) { //증가할 경우
+      //   month = parseInt(month);
+      //   if (whatmonths(month) === 0 && parseInt(dates) > 29) {
+      //     monAdd++;
+      //     dates = (Math.floor(parseInt(dates) % 30) + 1).toString();
+      //   }
+      //   else if (whatmonths(month) === 1 && parseInt(dates) > 30) {
+      //     monAdd++;
+      //     dates = (Math.floor(parseInt(dates) % 31) + 1).toString();
+      //   }
+      //   else if (whatmonths(month) === 2) {
 
-        }
-        else if (whatmonths(month) === 2) {
-
-        }
-        month = ('0' + month).toString();
-      }
+      //   }
+      //   dates = ('0' + dates).toString().slice(-2);
+      //   month = ('0' + (parseInt(month) === 0 ? 12 : parseInt(month) + monAdd)).toString();
+      // }
       setToday(parseInt(date.getFullYear().toString() + month + dates));
       const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${date.getFullYear().toString() + month + dates}`)).json();
       if (JSON.stringify(jsons) !== JSON.stringify({ "RESULT": { "CODE": "INFO-200", "MESSAGE": "해당하는 데이터가 없습니다." } }) && num < jsons.mealServiceDietInfo[1].row.length) {
@@ -82,21 +93,23 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return <div className="App">
-    <button onClick={() => {
-      if (num < 1) {
-        setAdd(c => c - 1);
-        fetching(add - 1, 2);
-        setNum(2);
-      }
-      else {
-        fetching(add, num - 1);
-        setNum(c => c - 1);
-      }
-    }}>◀</button>
-    <div>
+    <div className="left buttons">
+      <button onClick={() => {
+        if (num < 1) {
+          setAdd(c => c - 1);
+          fetching(add - 1, 2);
+          setNum(2);
+        }
+        else {
+          fetching(add, num - 1);
+          setNum(c => c - 1);
+        }
+      }}>◀</button>
+    </div>
+    <div className="MainContents">
       <p>{today}&nbsp;{num + 1}번째</p>
-      {loading === 1 ? <span>loading</span> : (loading === -1 ? <>급식이 없어요.</> : <>
-        <div className="Main">
+      {loading === 1 ? <span>loading</span> : (loading === -1 ? <div className="NotFound">급식이 없어요.</div> : <>
+        <div className="List">
           <p>{list[0].MLSV_TO_YMD}</p>
           <b>{list[num].MMEAL_SC_NM}&nbsp;</b>
           <span>총{list[num].CAL_INFO}</span>
@@ -104,17 +117,19 @@ function App() {
         </div>
       </>)}
     </div>
-    <button onClick={() => {
-      if (num > 1) {
-        setAdd(c => c + 1);
-        fetching(add + 1, 0);
-        setNum(0);
-      }
-      else {
-        fetching(add, num + 1);
-        setNum(c => c + 1);
-      }
-    }}>▶</button>
+    <div className="right buttons">
+      <button onClick={() => {
+        if (num > 1) {
+          setAdd(c => c + 1);
+          fetching(add + 1, 0);
+          setNum(0);
+        }
+        else {
+          fetching(add, num + 1);
+          setNum(c => c + 1);
+        }
+      }}>▶</button>
+    </div>
   </div >;
 }
 
