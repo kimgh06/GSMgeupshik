9c0a72dd260bd222c92993f136aace0048c61379
kkh061101 <kkh061101@naver.com>
2023-01-30 12:09:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./App.scss";
 
 function App() {
   const [list, setList] = useState([]);
@@ -34,40 +35,39 @@ function App() {
       let dates = ('0' + (parseInt(date.getDate()) + add).toString()).slice(-2).toString();
       let monAdd = 0;
       let month = ('0' + (date.getMonth() + 1)).slice(-2).toString();
-      // if (parseInt(dates) < 1) { //감소할 경우
-      //   monAdd--;
-      //   month = parseInt(month);
-      //   if (whatmonths(month - 1) === 0) {
-      //     dates = (30 - (Math.abs(parseInt(dates)) % 30)).toString();
-      //   }
-      //   else if (whatmonths(month - 1) === 1) {
+      if (parseInt(dates) < 1) { //감소할 경우
+        monAdd--;
+        month = parseInt(month);
+        if (whatmonths(month - 1) === 0) {
+          dates = (30 - (Math.abs(parseInt(dates)) % 30)).toString();
+        }
+        else if (whatmonths(month - 1) === 1) {
 
-      //   }
-      //   else if (whatmonths(month - 1) === 2) {
+        }
+        else if (whatmonths(month - 1) === 2) {
 
-      //   }
-      //   month = (month+ monAdd);
-      // }
-      // else if (parseInt(dates) > 29) { //증가할 경우
-      //   month = parseInt(month);
-      //   if (whatmonths(month) === 0 && date > 30) {
-      //     monAdd++;
-      //     dates = (Math.floor(parseInt(dates) / 30)).toString();
-      //   }
-      //   else if (whatmonths(month) === 1) {
+        }
+        month = (month + monAdd);
+      }
+      else if (parseInt(dates) > 29) { //증가할 경우
+        month = parseInt(month);
+        if (whatmonths(month) === 0 && date > 30) {
+          monAdd++;
+          dates = (Math.floor(parseInt(dates) / 30)).toString();
+        }
+        else if (whatmonths(month) === 1) {
 
-      //   }
-      //   else if (whatmonths(month) === 2) {
+        }
+        else if (whatmonths(month) === 2) {
 
-      //   }
-      //   month = ('0' + month).toString();
-      // }
+        }
+        month = ('0' + month).toString();
+      }
       setToday(parseInt(date.getFullYear().toString() + month + dates));
       const jsons = await (await fetch(`https://open.neis.go.kr/hub/mealServiceDietInfo?key=bb0f24af7fbc4bc896e2be32361cb2e4&Type=json&ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&MLSV_YMD=${date.getFullYear().toString() + month + dates}`)).json();
       if (JSON.stringify(jsons) !== JSON.stringify({ "RESULT": { "CODE": "INFO-200", "MESSAGE": "해당하는 데이터가 없습니다." } }) && num < jsons.mealServiceDietInfo[1].row.length) {
         setList(jsons.mealServiceDietInfo[1].row);
         setLoading(0);
-        console.log('asdf');
       } else {
         setLoading(-1);
         console.log('it has error');
@@ -93,15 +93,17 @@ function App() {
         setNum(c => c - 1);
       }
     }}>◀</button>
-    <p>{today}&nbsp;{num + 1}번째</p>
-    {loading === 1 ? <span>loading</span> : (loading === -1 ? <>급식이 없어요.</> : <>
-      <div className="Main">
-        <p>{list[0].MLSV_TO_YMD}</p>
-        <b>{list[num].MMEAL_SC_NM}&nbsp;</b>
-        <span>총{list[num].CAL_INFO}</span>
-        <div dangerouslySetInnerHTML={{ __html: list[num].DDISH_NM }} />
-      </div>
-    </>)}
+    <div>
+      <p>{today}&nbsp;{num + 1}번째</p>
+      {loading === 1 ? <span>loading</span> : (loading === -1 ? <>급식이 없어요.</> : <>
+        <div className="Main">
+          <p>{list[0].MLSV_TO_YMD}</p>
+          <b>{list[num].MMEAL_SC_NM}&nbsp;</b>
+          <span>총{list[num].CAL_INFO}</span>
+          <div dangerouslySetInnerHTML={{ __html: list[num].DDISH_NM }} />
+        </div>
+      </>)}
+    </div>
     <button onClick={() => {
       if (num > 1) {
         setAdd(c => c + 1);
